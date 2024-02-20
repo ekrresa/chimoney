4a42dc9d1740917f6a43ffb48601248337e019f3
Ochuko Ekrresa <kelvinekrresa@gmail.com>
2024-02-20 15:11:34 +0100

    Setup api

--- apps/api/src/server.ts
+++ apps/api/src/server.ts
@@ -1,8 +1,20 @@
 import 'dotenv/config'
 import express, { type Express } from 'express'
+import morgan from 'morgan'
+import cors from 'cors'
 
 export const createServer = (): Express => {
   const app = express()
+
+  app
+    .disable('x-powered-by')
+    .use(cors())
+    .use(morgan('dev'))
+    .use(express.urlencoded({ extended: true }))
+    .use(express.json())
+    .get('/', (req, res) => {
+      res.send('Hello World!')
+    })
 
   return app
 }
