92f38bd74a968b0b812ec12d1e6215bbe4a46c4f
Ochuko Ekrresa <kelvinekrresa@gmail.com>
2024-02-24 08:49:16 +0100

    Remove console

--- apps/api/src/env.ts
+++ apps/api/src/env.ts
@@ -2,11 +2,8 @@ import { createEnv } from '@t3-oss/env-core'
 import { z } from 'zod'
 import 'dotenv/config'
 
-console.log(typeof process.env.CI)
-
 export const env = createEnv({
   server: {
-    NODE_ENV: z.enum(['development', 'test', 'production']),
     DATABASE_URL: z.string().url(),
     ACCESS_TOKEN_SECRET: z.string().min(1),
     REFRESH_TOKEN_SECRET: z.string().min(1),
